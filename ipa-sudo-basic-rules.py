dea5ab7564b1142bd395f72213f95895bc8e6af2
Christian Stankowic <info@stankowic-development.net>
2016-06-14 13:55:15 +0200

    MySQL server / Icinga commands
    - added MySQL and Icinga(2) server commands
    - fixed camelcasing

--- ipa-sudo-basic-rules.py
+++ ipa-sudo-basic-rules.py
@@ -17,13 +17,13 @@ import subprocess
 
 #set logger and version
 LOGGER = logging.getLogger('ipa-sudo-basic-rules.py')
-vers = "0.1.0"
+vers = "0.1.1"
 
 
 
 def run_cmd(cmd=""):
 	#run the command, it's tricky!
-	if options.dry_run:
+	if options.dryRun:
 		#print what would be done
 		LOGGER.info("I'd like to execute the following command: {0}".format(cmd))
 	else:
@@ -55,14 +55,15 @@ def import_definitions():
 		"monitoring" : "Managing monitoring",
 		"ipa-client" : "Managing IPA clients",
 		"ipa-server" : "Managing IPA servers",
-		"rhn-client" : "Managing Enterprise Linux clients"
+		"rhn-client" : "Managing Enterprise Linux clients",
+		"mysql-server" : "Managing MySQL servers"
 	}
 	
 	#command defintions
 	cmds.update({"delegating" : ["/usr/sbin/visudo"]})
 	cmds.update({"drivers" : ["/sbin/modprobe", "/sbin/rmmod"]})
 	cmds.update({"editors" : ["/usr/bin/vi", "/usr/bin/vim"]})
-	cmds.update({"filemgmt" : ["/bin/cp", "/bin/mv", "/usr/bin/rsync", "/bin/rm", "/bin/ls", "/bin/echo", "/bin/cat", "/usr/bin/less", "/bin/more", "/usr/bin/tail"]})
+	cmds.update({"filemgmt" : ["/bin/cp", "/bin/mv", "/usr/bin/rsync", "/bin/rm", "/bin/ls", "/bin/echo", "/bin/cat", "/usr/bin/less", "/bin/more", "/usr/bin/tail", "/bin/df", "/bin/du"]})
 	cmds.update({"fileperm" : ["/bin/chgrp", "/bin/chmod", "/bin/chown"]})
 	cmds.update({"fileperm-acl" : ["/usr/bin/chacl", "/usr/bin/gefacl", "/usr/bin/setfacl"]})
 	cmds.update({"locate" : ["/usr/bin/updatedb"]})
@@ -75,13 +76,14 @@ def import_definitions():
 	cmds.update({"storage" : ["/bin/mount", "/bin/umount", "/sbin/fdisk", "/sbin/sfdisk", "/sbin/parted", "/sbin/partprobe", "/sbin/mkfs", "/sbin/mkfs.ext3", "/sbin/mkfs.ext4", "/sbin/mkfs.xfs", "/sbin/resize2fs", "/sbin/tune2fs", "/sbin/xfs_growfs", "/sbin/pvchange", "/sbin/pvcreate", "/sbin/pvdisplay", "/sbin/pvmove", "/sbin/pvremove", "/sbin/pvresize", "/sbin/pvs", "/sbin/pvscan", "/sbin/vgchange", "/sbin/vgcreate", "/sbin/vgdisplay", "/sbin/vgexport", "/sbin/vgextend", "/sbin/vgimport", "/sbin/vgreduce", "/sbin/vgremove", "/sbin/vgrename", "/sbin/vgs", "/sbin/vgscan", "/sbin/lvchange", "/sbin/lvcreate", "/sbin/lvdisplay", "/sbin/lvextend", "/sbin/lvreduce", "/sbin/lvremove", "/sbin/lvrename", "/sbin/lvresize", "/sbin/lvscan", "/sbin/lvs", "/usr/bin/rescan-scsi-bus.sh", "/usr/bin/scsi-rescan", "/sbin/multipath"]})
 	cmds.update({"su" : ["/bin/su", "/sbin/sulogin", "/sbin/sushell", "/sbin/runuser"]})
 	cmds.update({"usermgmt" : ["/usr/sbin/useradd", "/usr/sbin/userdel", "/usr/sbin/usermod", "/usr/sbin/groupadd", "/usr/sbin/groupdel", "/usr/sbin/groupmod", "/usr/bin/id", "/usr/bin/gpasswd", "/usr/bin/chage", "/bin/passwd", "/usr/bin/passwd", "/usr/bin/chfn", "/usr/bin/chsh", "/usr/sbin/vigr", "/usr/sbin/vipw"]})
-	cmds.update({"monitoring" : ["/usr/bin/omd"]})
+	cmds.update({"monitoring" : ["/usr/bin/omd","/usr/sbin/icinga2","/usr/bin/icingacli"]})
 	cmds.update({"ipa-client" : ["/usr/sbin/ipa-client-install", "/usr/sbin/ipa-client-automount", "/usr/sbin/ipa-certupdate", "/usr/bin/ipa-getcert", "/usr/sbin/ipa-getkeytab", "/usr/sbin/ipa-join", "/usr/sbin/ipa-rmkeytab"]})
 	cmds.update({"ipa-server" : ["/usr/bin/ipa", "/usr/sbin/ipa-ca-install", "/usr/sbin/ipa-csreplica-manage", "/usr/sbin/ipa-otptoken-import", "/usr/sbin/ipa-restore", "/usr/sbin/ipa-upgradeconfig", "/usr/sbin/ipa-adtrust-install", "/usr/sbin/ipactl", "/usr/sbin/ipa-kra-install", "/usr/sbin/ipa-replica-conncheck", "/usr/sbin/ipa-winsync-migrate", "/usr/sbin/ipa-advise", "/usr/sbin/ipa-dns-install", "/usr/sbin/ipa-ldap-updater", "/usr/sbin/ipa-replica-install", "/usr/sbin/ipa-server-certinstall", "/usr/sbin/ipa-backup", "/usr/sbin/ipa-managed-entries", "/usr/sbin/ipa-replica-manage", "/usr/sbin/ipa-server-install", "/usr/sbin/ipa-cacert-manage", "/usr/sbin/ipa-compat-manage", "/usr/sbin/ipa-nis-manage", "/usr/sbin/ipa-replica-prepare", "/usr/sbin/ipa-server-upgrade"]})
 	cmds.update({"rhn-client" : ["/usr/sbin/rhn_check", "/usr/sbin/rhnreg_ks", "/usr/bin/rhn_register", "/usr/bin/rhn-actions-control", "/usr/bin/rhncfg-client", "/usr/sbin/rhn-channel"]})
+	cmds.update({"mysql-server" : ["/usr/bin/mysqladmin", "/usr/bin/mysql_secure_installation", "/usr/bin/mysql_install_db"]})
 	
 	#print definition version:
-	if options.info_only == True:
+	if options.infoOnly == True:
 		total = [len(v) for v in cmds.values()]
 		counter=0
 		for i in total: counter += i
@@ -89,7 +91,7 @@ def import_definitions():
 		exit(0)
 	
 	#print definitions
-	if options.list_only == True:
+	if options.listOnly == True:
 		for group in cmd_groups:
 			LOGGER.info("Group '{0}' ({1}) has the following commands:".format(group, cmd_groups[group]))
 			LOGGER.info('  ' + ', '.join(cmds[group]))
@@ -107,7 +109,7 @@ def parse_options(args=None):
 	#define usage, description, version and load parser
 	desc='''%prog is used to import a basic set of sudo commands and command groups into an existing FreeIPA installation.
 
-Checkout the GitHub page for updates: https://github.com/stdevel/freeipa-stuff'''
+Checkout the GitHub page for updates: https://github.com/stdevel/freeipa-utils'''
 	parser = OptionParser(description=desc, version="%prog version {0}".format(vers))
 	#define option groups
 	genOpts = OptionGroup(parser, "Generic Options")
@@ -117,11 +119,11 @@ Checkout the GitHub page for updates: https://github.com/stdevel/freeipa-stuff''
 	#-d / --debug
 	genOpts.add_option("-d", "--debug", dest="debug", default=False, action="store_true", help="enable debugging outputs (default: no)")
 	#-n / --dry-run
-	genOpts.add_option("-n", "--dry-run", dest="dry_run", default=False, action="store_true", help="only simulates what the script would do (default: no)")
+	genOpts.add_option("-n", "--dry-run", dest="dryRun", default=False, action="store_true", help="only simulates what the script would do (default: no)")
 	#-i / --info-only
-	genOpts.add_option("-i", "--info-only", dest="info_only", default=False, action="store_true", help="only print definition version and quits (default: no)")
+	genOpts.add_option("-i", "--info-only", dest="infoOnly", default=False, action="store_true", help="only print definition version and quits (default: no)")
 	#-l / --list-only
-	genOpts.add_option("-l", "--list-only", dest="list_only", default=False, action="store_true", help="only prints definitions and quits (default: no)")
+	genOpts.add_option("-l", "--list-only", dest="listOnly", default=False, action="store_true", help="only prints definitions and quits (default: no)")
 	
 	#parse and return options and arguments
 	(options, args) = parser.parse_args(args)
