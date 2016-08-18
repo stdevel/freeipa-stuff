f4486216e19bc09ec3337c3137d72deb7ff06926
Christian Stankowic <info@stankowic-development.net>
2016-08-18 10:16:20 +0200

    Katello, power and bug commands
    Release 0.1.9:
    - added some yum-related commands
    - added Katello server and client commands
    - added some abrt bug reporting commands
    - added some power management commands

--- ipa-sudo-basic-rules.py
+++ ipa-sudo-basic-rules.py
@@ -17,7 +17,7 @@ import subprocess
 
 #set logger and version
 LOGGER = logging.getLogger('ipa-sudo-basic-rules.py')
-vers = "0.1.8"
+vers = "0.1.9"
 
 
 
@@ -58,13 +58,17 @@ def import_definitions():
 		"monitoring" : "Managing monitoring",
 		"ipa-client" : "Managing IPA clients",
 		"ipa-server" : "Managing IPA servers",
-		"rhn-server" : "Managing Red Hat Satellite",
-		"rhn-client" : "Managing Enterprise Linux clients",
+		"rhn-server" : "Managing Spacewalk servers",
+		"rhn-client" : "Managing Spacewalk clients",
+		"katello-server" : "Managing Katello servers",
+		"katello-client" : "Managing Katello clients",
 		"mysql-server" : "Managing MySQL servers",
 		"postfix" : "Managing Postfix servers",
 		"disk-quotas" : "Managing disk quotas",
 		"nfs-server" : "Managing NFS servers",
-		"nfs-client" : "Managing NFS mounts"
+		"nfs-client" : "Managing NFS mounts",
+		"power" : "Managing power",
+		"bugs" : "Managing bug reports"
 	}
 	
 	#command defintions
@@ -83,7 +87,7 @@ def import_definitions():
 	cmds.update({"selinux-files" : ["/sbin/setfiles", "/sbin/fixfiles", "/usr/bin/chcon", "/sbin/restorecon"]})
 	cmds.update({"services" : ["/sbin/service", "/bin/systemctl", "/sbin/chkconfig"]})
 	cmds.update({"shells" : ["/bin/bash", "/bin/csh", "/bin/dash", "/bin/ksh", "/bin/mksh", "/bin/sh", "/bin/tcsh", "/bin/zsh", "/usr/bin/scl", "/usr/bin/screen", "/usr/bin/tmux", "/bin/vi", "/bin/vim", "/bin/view", "/bin/find"]})
-	cmds.update({"software" : ["/bin/rpm", "/usr/bin/up2date", "/usr/bin/yum", "/usr/bin/dnf", "/usr/bin/package-cleanup", "/usr/sbin/rpmconf"]})
+	cmds.update({"software" : ["/bin/rpm", "/usr/bin/up2date", "/usr/bin/yum", "/usr/sbin/yum-complete-transaction", "/usr/bin/yumdownloader", "/usr/bin/yum-config-manager", "/usr/bin/dnf", "/usr/bin/package-cleanup", "/usr/sbin/rpmconf", "/usr/bin/needs-restarting"]})
 	cmds.update({"storage" : ["/bin/mount", "/bin/umount", "/sbin/fdisk", "/sbin/sfdisk", "/sbin/parted", "/sbin/partprobe", "/sbin/mkfs", "/sbin/mkfs.ext3", "/sbin/mkfs.ext4", "/sbin/mkfs.xfs", "/sbin/resize2fs", "/sbin/tune2fs", "/sbin/xfs_growfs", "/sbin/pvchange", "/sbin/pvcreate", "/sbin/pvdisplay", "/sbin/pvmove", "/sbin/pvremove", "/sbin/pvresize", "/sbin/pvs", "/sbin/pvscan", "/sbin/vgchange", "/sbin/vgcreate", "/sbin/vgdisplay", "/sbin/vgexport", "/sbin/vgextend", "/sbin/vgimport", "/sbin/vgreduce", "/sbin/vgremove", "/sbin/vgrename", "/sbin/vgs", "/sbin/vgscan", "/sbin/lvchange", "/sbin/lvcreate", "/sbin/lvdisplay", "/sbin/lvextend", "/sbin/lvreduce", "/sbin/lvremove", "/sbin/lvrename", "/sbin/lvresize", "/sbin/lvscan", "/sbin/lvs", "/usr/bin/rescan-scsi-bus.sh", "/usr/bin/scsi-rescan", "/sbin/multipath", "/sbin/badblocks"]})
 	cmds.update({"su" : ["/bin/su", "/sbin/sulogin", "/sbin/sushell", "/sbin/runuser"]})
 	cmds.update({"usermgmt" : ["/usr/sbin/useradd", "/usr/sbin/userdel", "/usr/sbin/usermod", "/usr/sbin/groupadd", "/usr/sbin/groupdel", "/usr/sbin/groupmod", "/usr/bin/id", "/usr/bin/gpasswd", "/usr/bin/chage", "/bin/passwd", "/usr/bin/passwd", "/usr/bin/chfn", "/usr/bin/chsh", "/usr/sbin/vigr", "/usr/sbin/vipw"]})
@@ -92,11 +96,15 @@ def import_definitions():
 	cmds.update({"ipa-server" : ["/usr/bin/ipa", "/usr/sbin/ipa-ca-install", "/usr/sbin/ipa-csreplica-manage", "/usr/sbin/ipa-otptoken-import", "/usr/sbin/ipa-restore", "/usr/sbin/ipa-upgradeconfig", "/usr/sbin/ipa-adtrust-install", "/usr/sbin/ipactl", "/usr/sbin/ipa-kra-install", "/usr/sbin/ipa-replica-conncheck", "/usr/sbin/ipa-winsync-migrate", "/usr/sbin/ipa-advise", "/usr/sbin/ipa-dns-install", "/usr/sbin/ipa-ldap-updater", "/usr/sbin/ipa-replica-install", "/usr/sbin/ipa-server-certinstall", "/usr/sbin/ipa-backup", "/usr/sbin/ipa-managed-entries", "/usr/sbin/ipa-replica-manage", "/usr/sbin/ipa-server-install", "/usr/sbin/ipa-cacert-manage", "/usr/sbin/ipa-compat-manage", "/usr/sbin/ipa-nis-manage", "/usr/sbin/ipa-replica-prepare", "/usr/sbin/ipa-server-upgrade"]})
 	cmds.update({"rhn-server" : ["/usr/sbin/rhn-satellite", "/usr/sbin/spacewalk-service", "/usr/bin/rhn-satellite-activate", "/usr/bin/rhn-satellite-exporter", "/usr/bin/rhn-schema-version", "/usr/bin/spacewalk-cfg-get", "/usr/bin/spacewalk-common-channels", "/usr/bin/spacewalk-data-fsck", "/usr/bin/spacewalk-debug", "/usr/bin/spacewalk-export", "/usr/bin/spacewalk-export-channels", "/usr/bin/spacewalk-hostname-rename", "/usr/bin/spacewalk-remove-channel", "/usr/bin/spacewalk-report", "/usr/bin/spacewalk-repo-sync", "/usr/bin/spacewalk-schema-upgrade", "/usr/bin/spacewalk-selinux-enable", "/usr/bin/spacewalk-setup", "/usr/bin/spacewalk-setup-cobbler", "/usr/bin/spacewalk-setup-ipa-authentication", "/usr/bin/spacewalk-setup-jabberd"]})
 	cmds.update({"rhn-client" : ["/usr/sbin/rhn_check", "/usr/sbin/rhnreg_ks", "/usr/bin/rhn_register", "/usr/bin/rhn-actions-control", "/usr/bin/rhncfg-client", "/usr/sbin/rhn-channel", "/usr/sbin/rhn-profile-sync", "/usr/bin/subscription-manager"]})
+	cmds.update({"katello-server" : ["/usr/bin/katello-service", "/usr/bin/katello-backup", "/usr/bin/katello-restore", "/usr/bin/katello-certs-sign", "/usr/bin/katello-certs-gen-rpm", "/usr/sbin/katello-certs-check", "/usr/sbin/katello-installer", "/usr/bin/katello-remove", "/usr/sbin/katello-selinux-disable", "/usr/sbin/katello-selinux-enable", "/usr/sbin/katello-selinux-relabel"]})
+	cmds.update({"katello-client" : ["/usr/bin/subscription-manager", "/usr/bin/katello-rhsm-consumer"]})
 	cmds.update({"mysql-server" : ["/usr/bin/mysqladmin", "/usr/bin/mysql_secure_installation", "/usr/bin/mysql_install_db"]})
 	cmds.update({"postfix" : ["/usr/bin/newaliases", "/usr/sbin/postalias", "/usr/sbin/postconf", "/usr/sbin/postfix", "/usr/sbin/postlock", "/usr/sbin/postmap", "/usr/sbin/postsuper"]})
 	cmds.update({"disk-quotas" : ["/sbin/edquota", "/sbin/quotaon", "/sbin/quotaoff", "/sbin/quotacheck", "/usr/bin/quota", "/usr/sbin/repquota", "/usr/sbin/setquota", "/usr/sbin/convertquota"]})
 	cmds.update({"nfs-server" : ["/usr/sbin/exportfs"]})
 	cmds.update({"nfs-client" : ["/sbin/mount.nfs", "/sbin/mount.nfs4", "/sbin/umount.nfs", "/sbin/umount.nfs4"]})
+	cmds.update({"power" : ["/usr/sbin/reboot", "/usr/sbin/poweroff", "/sbin/shutdown"]})
+	cmds.update({"bugs" : ["/usr/sbin/abrt-auto-reporting", "/usr/sbin/abrt-configuration", "/usr/bin/abrt-cli"]})
 	
 	#print definition version:
 	if options.info_only == True:
